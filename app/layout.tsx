f6c3b05b88cca69797a5a58d3c46d73678eaf6c8
JohnDoeBlocks <joao.oliveira@scoreplay.xyz>
2026-04-22 17:38:32 +0100

    add apollo

--- app/layout.tsx
+++ app/layout.tsx
@@ -192,6 +192,22 @@ export default async function RootLayout({
             });
           `}
         </Script>
+        <Script id="apollo-tracker" strategy="afterInteractive">
+          {`
+            function initApollo() {
+              var n = Math.random().toString(36).substring(7);
+              var o = document.createElement('script');
+              o.src = 'https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=' + n;
+              o.async = true;
+              o.defer = true;
+              o.onload = function() {
+                window.trackingFunctions.onLoad({ appId: '69e8f7a90f50e3001d12b881' });
+              };
+              document.head.appendChild(o);
+            }
+            initApollo();
+          `}
+        </Script>
 
         {/* Cookie Consent Manager */}
         <CookieConsent />
